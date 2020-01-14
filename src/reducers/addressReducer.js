21de1aab9dc60387d24d681dd97e7245c9f88c23
dennerruby <dennersk891@gmail.com>
2020-01-13 22:41:20 -0300

    direct change

---
+++ src/reducers/addressReducer.js
@@ -0,0 +1,14 @@
+
+import { SET_ADDRESS } from "../actions/action_types";
+ 
+export default (state = { address: [] }, action) => {
+  switch(action.type) {
+    case SET_ADDRESS:
+      return { 
+        ...state,
+        address: action.address
+      }
+    default:
+      return state
+  }
+}
